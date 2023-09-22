eb694560257c1997cdd83ce221d9b6424a0a48d1
CamilleGrubham <cngrubham@gmail.com>
2023-09-22 13:38:23 -0700

    require modules birds.js

---
+++ controllers/birds.js
@@ -0,0 +1,15 @@
+/* Require modules
+--------------------------------------------------------------- */
+const express = require("express");
+const router = express.Router();
+
+/* Require the db connection, and models
+--------------------------------------------------------------- */
+const db = require("../models");
+
+/* Routes
+--------------------------------------------------------------- */
+
+/* Export these routes so that they are accessible in `server.js`
+--------------------------------------------------------------- */
+module.exports = router;
