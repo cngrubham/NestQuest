04a61e88bdff17d615f48d63fa8cf2ea2f041f3c
CamilleGrubham <cngrubham@gmail.com>
2023-09-22 14:30:33 -0700

    sightings routes

--- controllers/sightings.js
+++ controllers/sightings.js
@@ -10,6 +10,54 @@ const db = require("../models");
 
 /* Routes
 --------------------------------------------------------------- */
+// Index Route (All sightings):
+// GET sightings/
+router.get("/", (req, res) => {
+  db.Bird.find({}, { sightings: true, _id: false }).then((bird) => {
+    // format query results to appear in one array,
+    // rather than an array of objects containing arrays
+    const flatList = [];
+    for (let bird of birds) {
+      flatList.push(...bird.sightings);
+    }
+    res.render("sightings/sightings-index", { sightings: flatList });
+  });
+});
+
+router.get("/new/:birdId", async (req, res) => {
+  const bird = await db.Bird.findById(req.params.birdId);
+  res.render("sightings/new-form", { bird: bird });
+});
+
+// Create Route: POST sightings/
+router.post("/create/:birdId", (req, res) => {
+  db.Bird.findByIdAndUpdate(
+    req.params.birdId,
+    { $push: { sightings: req.body } },
+    { new: true }
+  ).then((bird) => res.redirect("/birds/" + bird._id));
+});
+
+// Show Route: GET sightings/:id
+router.get("/:id", (req, res) => {
+  db.Bird.findOne(
+    { "sightings._id": req.params.id },
+    { "sightings.$": true, _id: false }
+  ).then((bird) => {
+    // format query results to appear in one object,
+    // rather than an object containing an array of one object
+    res.render("sightings/sighting-details", { sightings: bird.sightings[0] });
+  });
+});
+
+// Destroy Route: DELETE localhost:3000/sightings/:id
+router.delete("/:id", (req, res) => {
+  db.Bird.findOneAndUpdate(
+    { "sightings._id": req.params.id },
+    { $pull: { sightings: { _id: req.params.id } } },
+    { new: true }
+  ).then(() => res.redirect("/sightings"));
+});
 
 /* Export these routes so that they are accessible in `server.js`
 --------------------------------------------------------------- */
