442f9ae61dc1a457438158ad81d9b85a472577b9
CamilleGrubham <cngrubham@gmail.com>
2023-09-22 13:50:36 -0700

    set up bird-seed

---
+++ models/bird-seed.js
@@ -0,0 +1,2 @@
+const birds = [{}];
+module.exports = birds;
---
+++ models/sighting.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const sightingSchema = new mongoose.Schema({
+
+ });
+
+ module.exports = sightingSchema;
